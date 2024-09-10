f2515cc738190a6eb14db592f45e1fc76edf588e
Andrew Logemann <andrewclogemann@gmail.com>
2024-09-10 11:15:06 -0400

    calorie form added

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
+import pg from 'pg'
+const { Pool } = pg
 import express from 'express'
 const app = express()
+
+//connect to db
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    database: 'andrewlogemann'
+})
 
 //app config
 app.use(express.static('public'))
 app.set('view engine','ejs')
+app.use(express.urlencoded({ extended: true }));
 
 //routes
 app.get('/', (req,res) => {
     res.render('home')
+})
+
+app.post('/', (req,res) => {
+    console.log('post!')
+    res.redirect('/')
+})
+
+app.get('/new', (req, res) => {
+    res.render('new')
 })
 
 //start server
